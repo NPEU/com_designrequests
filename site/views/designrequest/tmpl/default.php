be7d56dc8742990f20271ad26b1cbae341f77c6e
Andy Kirk <andy@andykirk.net>
2023-04-26 16:31:13 +0100

    Update for v2.

--- site/views/designrequest/tmpl/default.php
+++ site/views/designrequest/tmpl/default.php
@@ -22,67 +22,75 @@ $skip = array(
     'desc'
 );
 ?>
-<h2><?php echo $page_title ?></h2>
+<div class="l-box  l-box--space--edge">
+    <h2><?php echo $page_title ?></h2>
 
-<div class="l-col-to-row--flush-edge-gutters">
-    <div class="l-col-to-row  l-col-to-row--gutter">
+    <div class="l-layout  l-row  l-gutter  l-flush-edge-gutter">
+        <div class="l-layout__inner">
 
-        <div class="ff-width-100--50--66-666 l-col-to-row__item">
-            <div class="d-bands  u-padding">
-                <h3>Description</h3>
+            <div class="l-box  ff-width-100--50--66-666">
                 <div>
-                <?php echo Markdown::defaultTransform($this->item->desc); ?>
+                    <h3>Description</h3>
+                    <div>
+                    <?php echo Markdown::defaultTransform($this->item->desc); ?>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="ff-width-100--50--33-333 l-col-to-row__item">
-            <div class="d-background  u-padding">
-                <?php foreach ($this->form->getFieldsets() as $name => $fieldset): ?>
-                <dl>
-                    <?php foreach ($this->form->getFieldset($name) as $field): if(!in_array($field->fieldname, $skip)): ?>
-                    <?php
-                        $label = JText::_($field->getAttribute('label'));
-                        $value = false;
-                        $key   = $field->fieldname;
-                        #echo $label . ' ' . $key;
-                        if (isset($this->item->$key)) {
-                            $value = $this->item->$key;
-                        } elseif (isset($this->item->customFieldItemsKey[$field->fieldname])) {
-                            $value = $this->item->customFieldItemsKey[$field->fieldname]->realvalue;
-                        } else {
-                            continue;
-                        }
+            <div class="l-box  ff-width-100--50--33-333">
+                <div class="d-background  l-box  l-box--space--edge">
+                    <?php foreach ($this->form->getFieldsets() as $name => $fieldset): ?>
+                    <div class="l-layout  l-row">
+                        <dl class="l-layout__inner">
+                            <?php foreach ($this->form->getFieldset($name) as $field): if(!in_array($field->fieldname, $skip)): ?>
+                            <?php
+                                $label = JText::_($field->getAttribute('label'));
+                                $value = false;
+                                $key   = $field->fieldname;
+                                #echo $label . ' ' . $key;
+                                if (isset($this->item->$key)) {
+                                    $value = $this->item->$key;
+                                } elseif (isset($this->item->customFieldItemsKey[$field->fieldname])) {
+                                    $value = $this->item->customFieldItemsKey[$field->fieldname]->realvalue;
+                                } else {
+                                    continue;
+                                }
 
-                        if ($field->getAttribute('dateformat', false)) {
-                            $value = date($field->getAttribute('dateformat'), strtotime($value));
-                        }
-                        if ($field->getAttribute('stringsearch', false) && $field->getAttribute('stringreplace', false)) {
-                            $search  = html_entity_decode($field->getAttribute('stringsearch'));
-                            $replace = html_entity_decode($field->getAttribute('stringreplace'));
-                            $value = preg_replace('#' . $search . '#', $replace, $value);
-                            $value = Markdown::defaultTransform($value);
-                            $value = trim(preg_replace(array('#^<p>#', '#</p>$#'), '', $value));
-                        }
-                    ?>
-                    <dt><?php echo $label ?></dt>
-                    <dd><?php echo $value ?></dd>
-                    <?php endif; endforeach; ?>
-                </dl>
+                                if ($field->getAttribute('dateformat', false)) {
+                                    $value = date($field->getAttribute('dateformat'), strtotime($value));
+                                }
+                                if ($field->getAttribute('stringsearch', false) && $field->getAttribute('stringreplace', false)) {
+                                    $search  = html_entity_decode($field->getAttribute('stringsearch'));
+                                    $replace = html_entity_decode($field->getAttribute('stringreplace'));
+                                    $value = preg_replace('#' . $search . '#', $replace, $value);
+                                    $value = Markdown::defaultTransform($value);
+                                    $value = trim(preg_replace(array('#^<p>#', '#</p>$#'), '', $value));
+                                }
+                            ?>
+                            <dt class="l-box  ff-width-100--30--33-333"><?php echo $label ?></dt>
+                            <dd class="l-box  l-box--space--block-end  ff-width-100--30--66-666"><?php echo $value ?></dd>
+                            <?php endif; endforeach; ?>
+                        </dl>
+                    </div>
                 </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
+
+            <p class="l-box l-layout  l-row  l-gutter  l-flush-edge-gutter">
+                <span class="l-layout__inner  c-utilitext">
+                    <span class="l-box">
+                        <a href="<?php echo JRoute::_('index.php?option=com_designrequests'); ?>">
+                            <svg focusable="false" aria-hidden="true" width="1.25em" height="1.25em" display="none"><use xlink:href="#icon-chevron-left"></use></svg>
+                            <span>Back</span>
+                        </a>
+                    </span>
+                    <span class="l-box u-text-align--right">
+                        <a href="<?php echo JRoute::_('index.php?option=com_designrequests&task=designrequest.edit&id=' . $this->item->id); ?>" class="u-space--left--s">
+                            <span><?php echo JText::_('COM_DESIGNREQUESTS_RECORDS_ACTION_EDIT'); ?></span>
+                            <svg focusable="false" aria-hidden="true" width="1.25em" height="1.25em" display="none"><use xlink:href="#icon-edit"></use></svg>
+                        </a>
+                    </span>
+                </span>
+            </p>
         </div>
     </div>
 </div>
-
-
-<p class="c-utilitext  u-text-size--medium">
-    <a href="<?php echo JRoute::_('index.php?option=com_designrequests'); ?>">
-        <svg display="none" focusable="false" class="icon" aria-hidden="true"><use xlink:href="#icon-chevron-left"></use></svg>
-        <span>Back</span>
-    </a>
-    
-    <a href="<?php echo JRoute::_('index.php?option=com_designrequests&task=designrequest.edit&id=' . $this->item->id); ?>" class="u-space--left--s">
-        <span><?php echo JText::_('COM_DESIGNREQUESTS_RECORDS_ACTION_EDIT'); ?></span>
-        <svg display="none" focusable="false" class="icon  u-space--left--xs" aria-hidden="true"><use xlink:href="#icon-edit"></use></svg>
-    </a>
-</p>
